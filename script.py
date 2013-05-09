084c782af81ce3ce7b1933e7998769fc9f0f552d
Akshay Ratan <akshayratan@gmail.com>
2013-05-09 13:55:21 +0530

    Cleaning the code a little bit

--- script.py
+++ script.py
@@ -3,8 +3,8 @@ import requests
 import pprint
 
 query_params = { 'apikey': '0fce6ca142d042dca0f49936c212fe1e',
-			     'phrase': 'election night'
-			     'per_page' : 3,
+			     'per_page': 3,
+			     'phrase': 'election night',	
 			     'state' : 'MD',
 		       }
 
