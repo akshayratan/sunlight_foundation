333f41895eb9e7087fe2611ec4f7a1bb07ef79d3
Akshay Ratan <akshayratan@gmail.com>
2013-05-09 13:47:56 +0530

    Limiting the query

--- script.py
+++ script.py
@@ -1,11 +1,15 @@
 #Copyright: Akshay Ratan <akshayratan@gmail.com>
 import requests
+import pprint
 
-query_params = { 'apikey': 'API_KEY',
-			     'phrase': 'fiscal cliff'
+query_params = { 'apikey': '0fce6ca142d042dca0f49936c212fe1e',
+			     'phrase': 'election night'
+			     'per_page' : 3,
+			     'state' : 'MD',
 		       }
 
 endpoint = 'http://capitolwords.org/api/text.json'
 response = requests.get(endpoint, params=query_params)
-request_url=response
-
+data=response.json
+pprint.pprint(data)
+ 
