d6b21552f64285b288b89d5ca1916284389ad86b
Akshay Ratan <akshayratan@gmail.com>
2013-05-14 23:28:55 +0530

    Fixed few errors along with sorted search

--- top_words.py
+++ top_words.py
@@ -1,14 +1,18 @@
 #Searching for Top Words and phrases
 import requests
+import pprint
 
 query_params = { 'apikey': 'API_KEY',
 				 'per_page': 3,
 		   		 'entity_type': 'month',
-                 'entity_value': '201210'
+                 'entity_value': '201210',
+		 'sort': 'count desc'
 		 		}
+
+#Sorting by count tag and in descending order
 
 endpoint= 'http://capitolwords.org/api/phrases.json'
 
 response = requests.get(endpoint, params=query_params)
 data=response.json
-
+pprint.pprint(data)
